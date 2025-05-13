d845aa5cc579dac75018b3927809e3b15144e22f
1Shibam <hehe.shibam@gmail.com>
2025-05-14 01:30:12 +0530

    adding upload as post screen

---
+++ lib/presentation/screens/admin/upload_as_post_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UploadAsPostScreen extends StatefulWidget {
+  const UploadAsPostScreen({super.key});
+
+  @override
+  State<UploadAsPostScreen> createState() => _UploadAsPostScreenState();
+}
+
+//on this screen we need to do the upload as post the resolved complaints...
+class _UploadAsPostScreenState extends State<UploadAsPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
